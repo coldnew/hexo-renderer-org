d72cc4ca2f680852c325e4b0593626155be90ddb
codefalling <code.falling@gmail.com>
2015-10-29 21:16:11 +0800

    add .jsbeautifyrc and format code

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var renderer = require('./lib/renderer');
 var assign = require('object-assign');
 
 // Init option
-hexo.config.org =  assign({
-  toc:false
+hexo.config.org = assign({
+  toc: false
 }, hexo.config.org);
 
 hexo.extend.renderer.register('org', 'html', renderer, true);
+
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
 
 var should = require('chai').should();
 
-describe('Org renderer', function(){
+describe('Org renderer', function() {
 
   // in index.js
   var assign = require('object-assign');
@@ -23,17 +23,22 @@ describe('Org renderer', function(){
   ].join('\n');
 
 
-  it('rendering with default config', function(){
-    var result = r({text: body});
+  it('rendering with default config', function() {
+    var result = r({
+      text: body
+    });
     result.should.eql(
       '<h2 id="header-1">Orgmode</h2>\n<h3 id="header-1-1">Why orgmode</h3>\n<pre class="prettyprint"><code class="language-js">  consol.log(&quot;hello&quot;);</code>\n</pre>\n&#x6211;&#x7F16;&#x4E0D;&#x4E0B;&#x53BB;&#x4E86;&#x3002;&#x3002;'
-    );  // I just copy right value here
+    ); // I just copy right value here
   });
 
-  it('rendering with org TOC is true', function () {
+  it('rendering with org TOC is true', function() {
     ctx.config.org.toc = true;
 
-    var result = r({text: body});
-    result.should.eql('<ul><li><a href="#header-1">Orgmode</a><ul><li><a href="#header-1-1">Why orgmode</a></li>\n</ul>\n</li>\n</ul>\n\n<h2 id="header-1">Orgmode</h2>\n<h3 id="header-1-1">Why orgmode</h3>\n<pre class="prettyprint"><code class="language-js">  consol.log(&quot;hello&quot;);</code>\n</pre>\n&#x6211;&#x7F16;&#x4E0D;&#x4E0B;&#x53BB;&#x4E86;&#x3002;&#x3002;');  // I just copy right value here
+    var result = r({
+      text: body
+    });
+    result.should.eql('<ul><li><a href="#header-1">Orgmode</a><ul><li><a href="#header-1-1">Why orgmode</a></li>\n</ul>\n</li>\n</ul>\n\n<h2 id="header-1">Orgmode</h2>\n<h3 id="header-1-1">Why orgmode</h3>\n<pre class="prettyprint"><code class="language-js">  consol.log(&quot;hello&quot;);</code>\n</pre>\n&#x6211;&#x7F16;&#x4E0D;&#x4E0B;&#x53BB;&#x4E86;&#x3002;&#x3002;'); // I just copy right value here
   });
 });
+

7a5afe2a54ff4de65c587176c13965ef218094d4
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-03-04 23:36:03 +0800

    use default's simple-test.org as test file
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- emacs/test.sh
+++ emacs/test.sh
@@ -1,1 +1,21 @@
-emacs -Q -nw  -l init.el --eval "(hexo-render-org \"test/test.org\" \"test/\" \"test.html\")"
+#!/usr/bin/env sh
+
+CACHE_DIR="cache"
+
+mkdir -p $CACHE_DIR > /dev/null 2>&1
+
+EMACS_LISP="
+  (let ((args '(:file         \"simple-test.org\"
+                :cache-dir    \"$CACHE_DIR\"
+                :output-file  \"simple-test.html\"
+                :htmlize      \"true\"
+                :theme        \"tango\"
+                :user-config  \"\"
+                )))
+      (hexo-render-org args))
+"
+
+EMACS_LISP=${EMACS_LISP//$'\n'/} # Remove all newlines.
+EMACS_LISP=${EMACS_LISP%$'\n'}   # Remove a trailing newline.
+
+emacs -Q -nw -l init.el --eval "${EMACS_LISP}"
